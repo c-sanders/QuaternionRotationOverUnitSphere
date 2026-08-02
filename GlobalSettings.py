3d4bb9810781b8be8380c71d8da8c7353ec363bc
Craig Sanders <craig.sanders.promotions@gmail.com>
2026-08-02 13:23:04 +1000

    Initial commit.

---
+++ GlobalSettings.py
@@ -0,0 +1,12 @@
+import colorsys
+
+
+# Global settings used by this program.
+
+utility_quaternion_rotation = "/home/craig/local/source_code/haskell/HaskellQuaternionRotation/dist-newstyle/build/x86_64-linux/ghc-9.14.1/quaternion-0.1.0.0/x/quaternion/build/quaternion/quaternion"
+verbose_operation           = "False"
+
+rgb_value = colorsys.hsv_to_rgb(1.0, 1.0, 1.0)
+
+plot_quaternion_pre_multiply_history = True
+plot_quaternion_rotated_history      = True
