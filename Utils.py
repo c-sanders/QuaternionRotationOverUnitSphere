77b9f88aca561a9661f6d1a0b264507368d0250b
Craig Sanders <craig.sanders.promotions@gmail.com>
2026-08-16 09:36:19 +1000

    Initial commit.

---
+++ Utils.py
@@ -0,0 +1,52 @@
+def format_component(
+
+        name,
+        space,
+        value
+) :
+
+    if value == 0 :
+
+        if space :
+
+            return f" {name}{abs(value):.3f}"
+
+        else :
+
+            return f"+{name}{abs(value):.3f}"
+
+    elif value < 0 :
+
+        if space :
+
+            return f"- {name}{abs(value):.3f}"
+
+        else :
+
+            return f"-{name}{abs(value):.3f}"
+
+    else :
+
+        if space :
+
+            return f"+ {name}{value:.3f}"
+
+        else :
+
+            return f"+{name}{value:.3f}"
+
+
+def generate_string(
+
+        scalar_value,
+        i_value,
+        j_value,
+        k_value
+) :
+
+    label = self.format_component("", False, scalar_value) + " "
+    label += f"{self.format_component('i', True, i_value)} "
+    label += f"{self.format_component('j', True, j_value)} "
+    label += f"{self.format_component('k', True, k_value)}"
+
+    return label
